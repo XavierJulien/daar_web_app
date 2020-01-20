87a9a8361b5b24bb9c61e1c3508e13ea61942450
XavierJulien <julienxvier774@github.com>
2020-01-20 12:38:19 +0100

    first commit

---
+++ src/main/java/com/hxsarl/daar_web_app/DaarWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.hxsarl.daar_web_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaarWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaarWebAppApplication.class, args);
+	}
+
+}

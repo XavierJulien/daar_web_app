1f08adc8f828d2566d3b0723e49138616cab51f0
XavierJulien <julienxvier774@github.com>
2020-02-08 13:43:16 +0100

    test heroku

--- src/main/java/com/hxsarl/daar_web_app/controllers/SearchController.java
+++ src/main/java/com/hxsarl/daar_web_app/controllers/SearchController.java
@@ -3,6 +3,7 @@ package com.hxsarl.daar_web_app.controllers;
 
 import java.io.IOException;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import com.hxsarl.daar_web_app.items.Search;
 public class SearchController {
 
 	@GetMapping("/search")
+	@CrossOrigin(origins = {"http://localhost:3000"})
 	public Search searchBook(@RequestParam(value = "word", defaultValue = "") String word) {
 		return new Search(word);
 	}
